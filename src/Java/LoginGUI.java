04664937f415efbabd2b541fd1def909fa959f61
Ahmad-S-Qureshi <ahmadsqureshi@arizona.edu>
2024-09-23 20:38:54 -0700

    Began Java files to convert to UML

---
+++ src/Java/LoginGUI.java
@@ -0,0 +1,8 @@
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+public class LoginGUI extends GUI {
+    public LoginGUI(JPanel panel, JLabel[] labels, JButton[] buttons) {
+        super(panel, labels, buttons);
+    }
+}
