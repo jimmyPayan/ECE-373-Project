324c01a22a1fcc6af631e23556a5484007b54105
Ahmad-S-Qureshi <ahmadsqureshi@arizona.edu>
2024-09-28 05:30:27 -0700

    Created database, encryptor, and decryptor

---
+++ src/Java/WeatherInfoPanel.java
@@ -0,0 +1,7 @@
+public class WeatherInfoPanel {
+    public static void main(String[] args) {
+        LoginGUI loginGUI = new LoginGUI();
+        loginGUI.placeOnPanel();
+        loginGUI.placeOnScreen();
+    }
+}
