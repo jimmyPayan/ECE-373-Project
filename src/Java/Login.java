e03267fa596904b63b6fd274adc3dbd79750a49d
Ahmad-S-Qureshi <ahmadsqureshi@arizona.edu>
2024-10-08 13:25:51 -0700

    Completed Login

--- src/Java/Login.java
+++ src/Java/Login.java
@@ -1,13 +1,15 @@
 public class Login {
     private LoginGUI gui;
-    private CredentialDatabase database;
+    private static CredentialDatabase database;
 
     public Login(LoginGUI gui, CredentialDatabase database) {
         this.gui = gui;
         this.database = database;
     } 
     public static void performLogin() {
-        System.out.println("Login button clicked! Perform login action here.");
+        if(database.inDatabase("Username", Encryptor.encrypt("Password", 1))) {
+            System.out.println("Password in system, go to next GUI");
+        }
     }
 
     
