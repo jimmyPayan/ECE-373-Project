8ca113856100b271f9b04a6596b94c9aa55e061f
Jimmy Payan <jimmypayan@arizona.edu>
2024-10-15 19:40:34 -0700

    Sorry for committing to main im not good at git lmfao

--- src/Java/User.java
+++ src/Java/User.java
@@ -1,10 +1,15 @@
 
 public class User {
     private String name;
+
     public User(String name) {
         this.name = name;
     }
-    public String getUser() {
+    public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
