3990d1ae38722e2080613cc3b3cc077073e11a20
Ahmad-S-Qureshi <ahmadsqureshi@arizona.edu>
2024-11-26 11:19:18 -0700

    login implementation

--- src/Java/Login.java
+++ src/Java/Login.java
@@ -1,14 +1,18 @@
+import java.io.File;
+
 public class Login {
     private LoginGUI gui;
-    private static CredentialDatabase database;
+    private static CredentialDatabase database = new CredentialDatabase(new File("DB.txt"));
 
     public Login(LoginGUI gui, CredentialDatabase database) {
         this.gui = gui;
         this.database = database;
     } 
-    public static void performLogin() {
-        if(database.inDatabase("Username", Encryptor.encrypt("Password", 1))) {
-            System.out.println("Password in system, go to next GUI");
+    public static void performLogin(String username, String password) {
+        if(database.inDatabase(username, Encryptor.encrypt(password, 1))) {
+            System.out.println("Account in system, go to next GUI");
+        } else {
+            System.out.println("Account does not exist");
         }
     }
 
--- src/Java/LoginGUI.java
+++ src/Java/LoginGUI.java
@@ -1,8 +1,14 @@
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.BoxLayout;
+import javax.swing.JTextField;
 public class LoginGUI extends GUI {
+    private JTextField usernameInput = new JTextField();
+    private JPasswordField passwordInput = new JPasswordField();
+
+
     public LoginGUI() {
         super(null, null, null);
         JPanel panel = new JPanel();
@@ -16,6 +22,7 @@ public class LoginGUI extends GUI {
         this.buttons = buttons;
         this.labels = labels;
     }
+    
     public void placeOnPanel() {
         panel.removeAll();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
@@ -24,8 +31,18 @@ public class LoginGUI extends GUI {
             panel.add(this.buttons[i]);
         }
         buttons[0].addActionListener(e -> {
-            Login.performLogin();
+            Login.performLogin(usernameInput.getText(), passwordInput.getText());
         });
-    }
 
+        panel.add(new JLabel("Username:"));
+        passwordInput.setEchoChar('-');
+        panel.add(this.usernameInput);
+        panel.add(new JLabel("Password:"));
+        panel.add(this.passwordInput);
+    }
+    public static void main(String[] args) {
+        LoginGUI gui = new LoginGUI();
+        gui.placeOnPanel();
+        gui.placeOnScreen();
+    }
 }
