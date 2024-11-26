0bdceb002b14518fba48beed44d79ad6b07d4982
jimmyPayan <jimmypayan@arizona.edu>
2024-11-26 15:24:10 -0700

    Added preliminary code for WeatherDisplayGUI. Not done but anyone who wants something to do? go for it

--- src/Java/WeatherDisplayGUI.java
+++ src/Java/WeatherDisplayGUI.java
@@ -2,7 +2,33 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 public class WeatherDisplayGUI extends GUI {
+    // Jimmy: Not done!!!
     public WeatherDisplayGUI(JPanel panel, JLabel[] labels, JButton[] buttons) {
-        super(panel, labels, buttons);
+        super(null, null, null);
+
+        JPanel = new JPanel();
+        JButton = new JButton[9];
+        
+        JButton[0]="Sunday";
+        JButton[1]="Monday";
+        JButton[2]="Tuesday";
+        JButton[3]="Wednesday";
+        JButton[4]="Thursday";
+        JButton[5]="Friday";
+        JButton[6]="Saturday";
+
+        JButton[7]="Exit";
+        JButton[8]="Main Menu";
+
+        // define labels
+        labels[0] = new JLabel("LoginGUI label");
+        labels[1] = new JLabel("DateGUI label");
+        labels[2] = new JLabel("UserSettingsGUI label");
+        labels[3] = new JLabel("ExitGUI label");
+
+        this.panel = panel;
+        panel.setBackground(Color.BLUE);
+        this.buttons = buttons;
+        this.labels = labels;
     }
 }
